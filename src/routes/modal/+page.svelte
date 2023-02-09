40b56e29f97411d76e94ae63055e2924a2b336b4
JongDeug <pinpoint1998@gmail.com>
2023-02-09 15:55:26 +0900

    Feat: Modal Component

---
+++ src/routes/modal/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Modal from "./Modal.svelte";
+
+    let showModal = false;
+    const handleToggleModal = () => {
+        showModal = !showModal;
+    }
+</script>
+
+<button on:click={handleToggleModal} class="bg-blue-500 rounded-full p-3 m-10 drop-shadow-md">Open Modal</button>
+<Modal open={showModal} on:cancle={handleToggleModal}/>
